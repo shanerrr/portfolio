02f76e4bbb60a07f4245b48eb4ea6e721fecd4ac
Shane Goonasekera <theshanukg@gmail.com>
2022-11-26 03:09:49 -0700

    hero page

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+  // text-[#B1CDEC]
+  return (
+    <div className="container mx-auto">
+      <span className="">👋 Hello, my name is</span>
+      <h1 className="text-7xl font-extrabold leading-tight text-yellow">
+        Shane Goonasekera.
+      </h1>
+      <h2 className="text-7xl font-bold leading-tight">
+        I love to build software 💻
+      </h2>
+      <p className="mt-3 text-xl">
+        I'm an Edmonton-based software engineer building frontend and
+        <br />
+        backend applications for the web. Currently, I am a student
+        <br />
+        in my last year finishing up my computer science degree.
+      </p>
+    </div>
+  );
+}
