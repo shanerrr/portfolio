12673c8b19f206556cceb2cdfffd8421ccda9502
Shane Goonasekera <theshanukg@gmail.com>
2022-11-25 23:52:53 -0700

    tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div
+      className={`${inter.variable} h-screen font-sans bg-gradient-to-r from-[#5f2c82] to-[#49a09d]`}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 }
