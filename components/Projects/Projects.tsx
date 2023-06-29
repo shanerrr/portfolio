331fbef671a0c68a14c9de607d0a5e65d17e6aad
Shane Goonasekera <theshanukg@gmail.com>
2023-06-28 21:28:26 -0600

    even and odd styling for prjects

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -48,31 +48,51 @@ export default function Projects() {
           {projects.data
             .filter((project) => project.isSpotlight)
             .map((project) => (
-              <motion.li variants={childVariants} key={project.name}>
+              <motion.li
+                variants={childVariants}
+                key={project.name}
+                className="group"
+              >
                 <div className="grid h-96 grid-cols-12 grid-rows-1">
-                  <div className="relative z-30 col-start-7 col-end-12 row-span-full text-right">
+                  <div className="relative z-30 row-span-full group-odd:col-start-1 group-odd:col-end-7 group-even:col-start-7 group-even:col-span-full group-even:text-right">
                     <div className="flex h-full flex-col justify-between">
-                      <div>
-                        <h3 className="text-md inline self-start rounded-3xl bg-yellow/10 px-4 py-2 leading-loose text-yellow">
+                      <div className="flex group-even:justify-end">
+                        <span className="text-md inline self-start rounded-3xl bg-yellow/10 px-4 py-2 leading-loose text-yellow">
                           Spotlight Project
-                        </h3>
-                        <h1 className="mt-2 text-3xl font-bold">
-                          {project.name}
-                        </h1>
-                        <h4 className="text-xs text-yellow opacity-40">
-                          {project.time}
-                        </h4>
+                        </span>
+                      </div>
+                      <div>
+                        <div className="flex py-3 group-odd:flex-row-reverse group-odd:justify-end group-even:justify-end">
+                          <div>
+                            <h1 className="text-3xl font-bold">
+                              {project.name}
+                            </h1>
+                            <h4 className="text-xs text-yellow opacity-40">
+                              {project.time}
+                            </h4>
+                          </div>
+                          <div className="relative w-14 overflow-hidden rounded-full group-odd:mr-4 group-even:ml-4">
+                            <Image
+                              className=" object-cover"
+                              src="Image"
+                              quality={100}
+                              fill={true}
+                              alt={project.name}
+                              loader={() => project.logo}
+                            />
+                          </div>
+                        </div>
                       </div>
                       <p className="my-4 rounded-2xl bg-brightgreen p-4 text-lg transition-shadow duration-150 hover:shadow-lg">
                         {project.description}
                       </p>
                       <div>
-                        <div className="flex justify-end gap-3 overflow-x-auto text-sm opacity-60 ">
+                        <div className="flex gap-3 overflow-x-auto text-sm opacity-60 group-even:justify-end ">
                           {project.tags.map((tag) => (
                             <span key={tag}>{tag}</span>
                           ))}
                         </div>
-                        <div className="mt-3 flex justify-end gap-4 text-xl">
+                        <div className="mt-3 flex gap-4 text-xl group-even:justify-end">
                           {project.repo && (
                             <a
                               target="_blank"
@@ -93,7 +113,7 @@ export default function Projects() {
                       </div>
                     </div>
                   </div>
-                  <div className="relative col-start-1 col-end-8 row-span-full h-full overflow-hidden rounded-2xl">
+                  <div className="relative row-span-full h-full overflow-hidden rounded-2xl group-odd:col-start-6 group-odd:col-span-full group-even:col-start-1 group-even:col-end-8">
                     <div className="absolute z-20 h-full w-full bg-yellow/30 opacity-100 transition-opacity duration-150 hover:opacity-0" />
                     <Image
                       className=" object-cover"
@@ -103,50 +123,6 @@ export default function Projects() {
                     />
                   </div>
                 </div>
-                {/* <div className="flex justify-between">
-                  <Image
-                    className="rounded-full object-contain"
-                    src="Image"
-                    width={75}
-                    height={75}
-                    quality={100}
-                    alt={project.name}
-                    loader={() => project.logo}
-                  />
-                  <h3 className="text-md inline self-start rounded-3xl bg-yellow/10 px-4 py-2 leading-loose text-yellow">
-                    Spotlight Project
-                  </h3>
-                </div>
-                <h1 className="mt-3 text-3xl font-bold">{project.name}</h1>
-                <h4 className="text-xs text-yellow opacity-40">
-                  {project.time}
-                </h4>
-                <p className="mt-2 text-lg">{project.description}</p>
-                <div className="grid grid-cols-4 items-center justify-between py-4">
-                  <div className="col-span-3 flex gap-3 overflow-x-auto text-sm opacity-60">
-                    {project.tags.map((tag) => (
-                      <span key={tag}>{tag}</span>
-                    ))}
-                  </div>
-                  <div className="col-span-1 flex justify-end gap-4 text-2xl">
-                    {project.repo && (
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="fa-brands fa-github transition-colors duration-150 hover:text-yellow"
-                        href={project.repo as string}
-                      />
-                    )}
-                    {project.external && (
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="fa-regular fa-arrow-up-right-from-square transition-colors duration-150 hover:text-yellow"
-                        href={project.external as string}
-                      />
-                    )}
-                  </div>
-                </div> */}
               </motion.li>
             ))}
         </ul>
